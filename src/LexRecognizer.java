68462d0db8ae4eea609c739237adb218f4915d9d
Zetsu1113 <squidlover001@gmail.com>
2016-02-25 18:16:17 +0800

    Refactored function methods to be followed
    - changed the body of keyword in LexRecognizer
    - change the body of comment in LexRecognizer

--- src/LexRecognizer.java
+++ src/LexRecognizer.java
@@ -3,58 +3,38 @@ public class LexRecognizer {
 	
 	char charSet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'I', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ';' , '=', '+', '-', '*', '/', '\\' , '&', '<', '>' , '(' , ')', '{' , '}', '[', ']' , '#' , ':' ,'\'', '.' , ',' , '%', '^', '!', '@', '~' , '$',  '\"', ' ', '\t' , '\n'};
 
-	public static Token keyword(String buffer, int characterAt)
+	public static Token keyword(String str)
 	{
 		Token token = new Token();
-		token.tokenName="KEYWORD";
-		token.tokenAttribute="";
-		int tempCharAt = characterAt;
+		token.tokenName = "KEYWORD";
+		token.tokenAttribute = str;
+		boolean valid = true;
 		
-		if (buffer.charAt(characterAt) == '#')
+		for (int i = 0; i < str.length(); i++)
 		{
-			while(buffer.charAt(tempCharAt) != ' ' && buffer.charAt(tempCharAt) != '\n')
-			{
-				token.tokenName += buffer.charAt(tempCharAt);
-				tempCharAt++;
-			}
-			/*
-			while(buffer.charAt(tempCharAt) != '@')
-			{
-				tempCharAt++;
-			}
-			
-			while(buffer.charAt(tempCharAt) != ' ' && buffer.charAt(tempCharAt) != '\n')
-			{
-				token.tokenAttribute += buffer.charAt(tempCharAt);
-				tempCharAt++;
-			}*/
+			//if (//str.charAt(i)) //pattern
+				//continue;
+			//else
+			//{
+				//valid = false;
+				//break;
+			//}
 		}
+		
+		if (!valid)
+			token.tokenName = "ERROR";
+			
 		return token;
 	}
 	
-	public static Token comment(String buffer, int characterAt)
+	public static Token comment(String str)
 	{
 		Token token = new Token();
 		token.tokenName="COMMENT";
-		token.tokenAttribute="";
-		if (buffer.charAt(characterAt) == ':' && buffer.charAt(characterAt+1) == '/')
-		{
-			int tempCharAt = characterAt+2;
-			while(buffer.charAt(tempCharAt) != '\n')
-			{
-				token.tokenAttribute += buffer.charAt(tempCharAt);
-				tempCharAt++;
-			}
-		}
-		if (buffer.charAt(characterAt) == '(' && buffer.charAt(characterAt+1) == ':')
-		{
-			int tempCharAt = characterAt+2;
-			while(buffer.charAt(tempCharAt) != ':' || buffer.charAt(tempCharAt+1) != ')')
-			{
-				token.tokenAttribute += buffer.charAt(tempCharAt);
-				tempCharAt++;
-			}
-		}
+		token.tokenAttribute= str;
+		
+		if (!(str.charAt(0) == ':' && str.charAt(1) == '/') || !(str.charAt(0) == '(' && str.charAt(1) == ':' && str.charAt(str.length() - 1) == ':' && str.charAt(str.length() - 2) == ')'))	
+			token.tokenName = "ERROR";
 		return token;
 	}
 }
