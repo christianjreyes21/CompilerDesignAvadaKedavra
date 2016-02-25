19bb309fec68277e19f0cdf576566a9449bceb5f
christianjreyes21 <christianj.reyes21@gmail.com>
2016-02-25 13:25:45 +0800

    Fixed the format

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -3,30 +3,21 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
 
-
 public class InputOutput {
 
-	public static String getText(String FileName)
-	{
+	public static String getText(String FileName) {
 		String details = "";
-		try
-		{
-		File myFile = new File (FileName);
-		BufferedReader bf = new BufferedReader(new FileReader(myFile));
-		String buffer;
-		
-		
-		while ((buffer = bf.readLine()) != null)
-		{
-			details += buffer + "\n";
-		}
-		
-		
-		bf.close();
-		
-		}
-		catch (Exception e)
-		{
+		try {
+			File myFile = new File(FileName);
+			BufferedReader bf = new BufferedReader(new FileReader(myFile));
+			String buffer;
+
+			while ((buffer = bf.readLine()) != null) {
+				details += buffer + "\n";
+			}
+
+			bf.close();
+		} catch (Exception e) {
 			System.out.println("FILE NOT FOUND");
 		}
 		return details;
