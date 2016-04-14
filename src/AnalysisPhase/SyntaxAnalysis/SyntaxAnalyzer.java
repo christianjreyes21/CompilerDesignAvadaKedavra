d21f3bbbc3805cf254062bb22b5310c2b7788116
Zetsu1113 <squidlover001@gmail.com>
2016-04-15 02:18:35 +0800

    Fix

--- src/AnalysisPhase/SyntaxAnalysis/SyntaxAnalyzer.java
+++ src/AnalysisPhase/SyntaxAnalysis/SyntaxAnalyzer.java
@@ -1,6 +1,7 @@
 package AnalysisPhase.SyntaxAnalysis;
 
-import javax.swing.tree.DefaultMutableTreeNode;
+import Utilities.Node;
+
 
 
 public class SyntaxAnalyzer {
@@ -8,45 +9,45 @@ public class SyntaxAnalyzer {
 	// in order na to according sa documentation
 	// in order din sa kung paano gagawing yung functions (R.I.P)
 	
-	DefaultMutableTreeNode programNode = null;
-	DefaultMutableTreeNode functionNode = null;
-	DefaultMutableTreeNode mainFunctionNode = null;
-	DefaultMutableTreeNode subFunctionsNode = null;
-	DefaultMutableTreeNode subFunctionNode = null;
-	DefaultMutableTreeNode paramsNode = null;
-	DefaultMutableTreeNode statementsNode = null;
-	DefaultMutableTreeNode moreStatementNode = null;
-	DefaultMutableTreeNode statementNode = null;
-	DefaultMutableTreeNode declarationNode = null;
-	DefaultMutableTreeNode assignmentNode = null;
-	DefaultMutableTreeNode ioNode = null;
-	DefaultMutableTreeNode controlFlowNode = null;
-	DefaultMutableTreeNode expressionStatementsNode = null;
-	DefaultMutableTreeNode returnNode = null;
-	DefaultMutableTreeNode dataTypeNode = null;
-	DefaultMutableTreeNode inputStatement = null;
-	DefaultMutableTreeNode outputStatement = null;
-	DefaultMutableTreeNode conditionalNode = null;
-	DefaultMutableTreeNode loopingNode = null;
-	DefaultMutableTreeNode expressionsNode = null;
-	DefaultMutableTreeNode callParametersNode = null;
-	DefaultMutableTreeNode switchStatement = null;
-	DefaultMutableTreeNode forStatement = null;
-	DefaultMutableTreeNode incrementStatement = null;
-	DefaultMutableTreeNode decrementStatement = null;
-	DefaultMutableTreeNode postIncrementStatement = null;
-	DefaultMutableTreeNode postDecrementStatement = null;
-	DefaultMutableTreeNode mathExpressionStatement = null;
-	DefaultMutableTreeNode mathExpression2Statement = null;
-	DefaultMutableTreeNode mathExpression3Statement = null;
-	DefaultMutableTreeNode mathExpression4Statement = null;
-	DefaultMutableTreeNode stringExpressionStatement = null;
-	DefaultMutableTreeNode relationalExpressionStatement = null;
-	DefaultMutableTreeNode relationalExpression2Statement = null;
-	DefaultMutableTreeNode relationalExpression3Statement = null;
-	DefaultMutableTreeNode relationalExpression4Statement = null;
-	DefaultMutableTreeNode relationalExpression5Statement = null;
-	DefaultMutableTreeNode valueNode = null;
-	DefaultMutableTreeNode constantNode = null;
-	DefaultMutableTreeNode booleanConstantNode = null;
+	Node programNode = null;
+	Node functionNode = null;
+	Node mainFunctionNode = null;
+	Node subFunctionsNode = null;
+	Node subFunctionNode = null;
+	Node paramsNode = null;
+	Node statementsNode = null;
+	Node moreStatementNode = null;
+	Node statementNode = null;
+	Node declarationNode = null;
+	Node assignmentNode = null;
+	Node ioNode = null;
+	Node controlFlowNode = null;
+	Node expressionStatementsNode = null;
+	Node returnNode = null;
+	Node dataTypeNode = null;
+	Node inputStatement = null;
+	Node outputStatement = null;
+	Node conditionalNode = null;
+	Node loopingNode = null;
+	Node expressionsNode = null;
+	Node callParametersNode = null;
+	Node switchStatement = null;
+	Node<String> forStatement = null;
+	Node incrementStatement = null;
+	Node decrementStatement = null;
+	Node postIncrementStatement = null;
+	Node postDecrementStatement = null;
+	Node mathExpressionStatement = null;
+	Node mathExpression2Statement = null;
+	Node mathExpression3Statement = null;
+	Node mathExpression4Statement = null;
+	Node stringExpressionStatement = null;
+	Node relationalExpressionStatement = null;
+	Node relationalExpression2Statement = null;
+	Node relationalExpression3Statement = null;
+	Node relationalExpression4Statement = null;
+	Node relationalExpression5Statement = null;
+	Node valueNode = null;
+	Node constantNode = null;
+	Node booleanConstantNode = null;
 }
