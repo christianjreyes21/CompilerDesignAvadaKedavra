b82d43e33d0f3b49a2f1e6803a1062330898361b
Zetsu1113 <squidlover001@gmail.com>
2016-04-15 00:50:13 +0800

    Declarations Added
    - declarations of the non-terminals of the CFG added.

--- src/SyntaxAnalyzer.java
+++ src/SyntaxAnalyzer.java
@@ -1,4 +1,50 @@
+import javax.swing.tree.DefaultMutableTreeNode;
+
 
 public class SyntaxAnalyzer {
 
+	// in order na to according sa documentation
+	// in order din sa kung paano gagawing yung functions (R.I.P)
+	
+	DefaultMutableTreeNode programNode = null;
+	DefaultMutableTreeNode functionNode = null;
+	DefaultMutableTreeNode mainFunctionNode = null;
+	DefaultMutableTreeNode subFunctionsNode = null;
+	DefaultMutableTreeNode subFunctionNode = null;
+	DefaultMutableTreeNode paramsNode = null;
+	DefaultMutableTreeNode statementsNode = null;
+	DefaultMutableTreeNode moreStatementNode = null;
+	DefaultMutableTreeNode statementNode = null;
+	DefaultMutableTreeNode declarationNode = null;
+	DefaultMutableTreeNode assignmentNode = null;
+	DefaultMutableTreeNode ioNode = null;
+	DefaultMutableTreeNode controlFlowNode = null;
+	DefaultMutableTreeNode expressionStatementsNode = null;
+	DefaultMutableTreeNode returnNode = null;
+	DefaultMutableTreeNode dataTypeNode = null;
+	DefaultMutableTreeNode inputStatement = null;
+	DefaultMutableTreeNode outputStatement = null;
+	DefaultMutableTreeNode conditionalNode = null;
+	DefaultMutableTreeNode loopingNode = null;
+	DefaultMutableTreeNode expressionsNode = null;
+	DefaultMutableTreeNode callParametersNode = null;
+	DefaultMutableTreeNode switchStatement = null;
+	DefaultMutableTreeNode forStatement = null;
+	DefaultMutableTreeNode incrementStatement = null;
+	DefaultMutableTreeNode decrementStatement = null;
+	DefaultMutableTreeNode postIncrementStatement = null;
+	DefaultMutableTreeNode postDecrementStatement = null;
+	DefaultMutableTreeNode mathExpressionStatement = null;
+	DefaultMutableTreeNode mathExpression2Statement = null;
+	DefaultMutableTreeNode mathExpression3Statement = null;
+	DefaultMutableTreeNode mathExpression4Statement = null;
+	DefaultMutableTreeNode stringExpressionStatement = null;
+	DefaultMutableTreeNode relationalExpressionStatement = null;
+	DefaultMutableTreeNode relationalExpression2Statement = null;
+	DefaultMutableTreeNode relationalExpression3Statement = null;
+	DefaultMutableTreeNode relationalExpression4Statement = null;
+	DefaultMutableTreeNode relationalExpression5Statement = null;
+	DefaultMutableTreeNode valueNode = null;
+	DefaultMutableTreeNode constantNode = null;
+	DefaultMutableTreeNode booleanConstantNode = null;
 }
