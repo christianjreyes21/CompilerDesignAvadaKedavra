caad89fda3069a2f5a4b16e35c780afcc2dacb6d
MrElijahCenteno <2013060635@ust-ics.mygbiz.com>
2016-04-16 08:59:47 +0800

    Fixed Everything

--- src/AnalysisPhase/LexicalAnalysis/LexRecognizer.java
+++ src/AnalysisPhase/LexicalAnalysis/LexRecognizer.java
@@ -1,4 +1,4 @@
-package AnalysisPhase.LexicalAnalysis;
+import Token;
 
 public class LexRecognizer {
 
@@ -585,40 +585,52 @@ public class LexRecognizer {
 		token.tokenName = "ERROR";
 		token.tokenAttribute = str;
 		token.lineNumber = line;
-		//boolean valid = true;
+		boolean valid = true;
 
-		if ((str.length() == 1)
-				&& (str.charAt(0) == 'A' || str.charAt(0) == 'B'
-						|| str.charAt(0) == 'C' || str.charAt(0) == 'D'
-						|| str.charAt(0) == 'E' || str.charAt(0) == 'F'
-						|| str.charAt(0) == 'G' || str.charAt(0) == 'H'
-						|| str.charAt(0) == 'I' || str.charAt(0) == 'J'
-						|| str.charAt(0) == 'K' || str.charAt(0) == 'L'
-						|| str.charAt(0) == 'M' || str.charAt(0) == 'N'
-						|| str.charAt(0) == 'O' || str.charAt(0) == 'P'
-						|| str.charAt(0) == 'Q' || str.charAt(0) == 'R'
-						|| str.charAt(0) == 'S' || str.charAt(0) == 'T'
-						|| str.charAt(0) == 'U' || str.charAt(0) == 'V'
-						|| str.charAt(0) == 'W' || str.charAt(0) == 'X'
-						|| str.charAt(0) == 'Y' || str.charAt(0) == 'Z'
-						|| str.charAt(0) == 'a' || str.charAt(0) == 'b'
-						|| str.charAt(0) == 'c' || str.charAt(0) == 'd'
-						|| str.charAt(0) == 'e' || str.charAt(0) == 'f'
-						|| str.charAt(0) == 'g' || str.charAt(0) == 'h'
-						|| str.charAt(0) == 'i' || str.charAt(0) == 'j'
-						|| str.charAt(0) == 'k' || str.charAt(0) == 'l'
-						|| str.charAt(0) == 'm' || str.charAt(0) == 'n'
-						|| str.charAt(0) == 'o' || str.charAt(0) == 'p'
-						|| str.charAt(0) == 'q' || str.charAt(0) == 'r'
-						|| str.charAt(0) == 's' || str.charAt(0) == 't'
-						|| str.charAt(0) == 'u' || str.charAt(0) == 'v'
-						|| str.charAt(0) == 'w' || str.charAt(0) == 'x'
-						|| str.charAt(0) == 'y' || str.charAt(0) == 'z')){
+		System.out.println(str.charAt(0) + " " + str.charAt(1) + " " + str.charAt(2));
+		if ((str.length() == 3)
+				&& (str.charAt(1) == 'A' || str.charAt(1) == 'B'
+						|| str.charAt(1) == 'C' || str.charAt(1) == 'D'
+						|| str.charAt(1) == 'E' || str.charAt(1) == 'F'
+						|| str.charAt(1) == 'G' || str.charAt(1) == 'H'
+						|| str.charAt(1) == 'I' || str.charAt(1) == 'J'
+						|| str.charAt(1) == 'K' || str.charAt(1) == 'L'
+						|| str.charAt(1) == 'M' || str.charAt(1) == 'N'
+						|| str.charAt(1) == 'O' || str.charAt(1) == 'P'
+						|| str.charAt(1) == 'Q' || str.charAt(1) == 'R'
+						|| str.charAt(1) == 'S' || str.charAt(1) == 'T'
+						|| str.charAt(1) == 'U' || str.charAt(1) == 'V'
+						|| str.charAt(1) == 'W' || str.charAt(1) == 'X'
+						|| str.charAt(1) == 'Y' || str.charAt(1) == 'Z'
+						|| str.charAt(1) == 'a' || str.charAt(1) == 'b'
+						|| str.charAt(1) == 'c' || str.charAt(1) == 'd'
+						|| str.charAt(1) == 'e' || str.charAt(1) == 'f'
+						|| str.charAt(1) == 'g' || str.charAt(1) == 'h'
+						|| str.charAt(1) == 'i' || str.charAt(1) == 'j'
+						|| str.charAt(1) == 'k' || str.charAt(1) == 'l'
+						|| str.charAt(1) == 'm' || str.charAt(1) == 'n'
+						|| str.charAt(1) == 'o' || str.charAt(1) == 'p'
+						|| str.charAt(1) == 'q' || str.charAt(1) == 'r'
+						|| str.charAt(1) == 's' || str.charAt(1) == 't'
+						|| str.charAt(1) == 'u' || str.charAt(1) == 'v'
+						|| str.charAt(1) == 'w' || str.charAt(1) == 'x'
+						|| str.charAt(1) == 'y' || str.charAt(1) == 'z'
+						|| str.charAt(1) == '0' || str.charAt(1) == '1'
+						|| str.charAt(1) == '2' || str.charAt(1) == '3'
+						|| str.charAt(1) == '4' || str.charAt(1) == '5'
+						|| str.charAt(1) == '6' || str.charAt(1) == '7'
+						|| str.charAt(1) == '8' || str.charAt(1) == '9'
+						|| str.charAt(1) == '+' || str.charAt(1) == '-'
+						|| str.charAt(1) == '/' || str.charAt(1) == '='
+						|| str.charAt(1) == '(' || str.charAt(1) == '*'
+						|| str.charAt(1) == ' ' || str.charAt(1) == ')'
+						|| str.charAt(1) == '.' || str.charAt(1) == '_'
+						|| str.charAt(1) == '!' || str.charAt(1) == ',') && str.charAt(2) == '\''){
 			token.tokenName = "CHARACTER";
 		}
 		else {
-			for (int i = 0; i < str.length(); i++) {
-				if (!(str.charAt(i) == 'A' || str.charAt(i) == 'B'
+			for (int i = 1; i < str.length() - 1; i++) {
+				if ((str.charAt(i) == 'A' || str.charAt(i) == 'B'
 						|| str.charAt(i) == 'C' || str.charAt(i) == 'D'
 						|| str.charAt(i) == 'E' || str.charAt(i) == 'F'
 						|| str.charAt(i) == 'G' || str.charAt(i) == 'H'
@@ -643,8 +655,25 @@ public class LexRecognizer {
 						|| str.charAt(i) == 's' || str.charAt(i) == 't'
 						|| str.charAt(i) == 'u' || str.charAt(i) == 'v'
 						|| str.charAt(i) == 'w' || str.charAt(i) == 'x'
-						|| str.charAt(i) == 'y' || str.charAt(i) == 'z')) {
+						|| str.charAt(i) == 'y' || str.charAt(i) == 'z'
+						|| str.charAt(i) == '0' || str.charAt(i) == '1'
+						|| str.charAt(i) == '2' || str.charAt(i) == '3'
+						|| str.charAt(i) == '4' || str.charAt(i) == '5'
+						|| str.charAt(i) == '6' || str.charAt(i) == '7'
+						|| str.charAt(i) == '8' || str.charAt(i) == '9'
+						|| str.charAt(i) == '+' || str.charAt(i) == '-'
+						|| str.charAt(i) == '/' || str.charAt(i) == '='
+						|| str.charAt(i) == '(' || str.charAt(i) == '*'
+						|| str.charAt(i) == ' ' || str.charAt(i) == ')'
+						|| str.charAt(i) == '.' || str.charAt(i) == '_'
+						|| str.charAt(i) == '!' || str.charAt(i) == ',') && str.charAt(str.length() - 1) == '\'') {
 					//valid = false;
+					//break;
+					token.tokenName = "STRING";
+				}
+				else
+				{
+					token.tokenName = "ERROR";
 					break;
 				}
 			}
